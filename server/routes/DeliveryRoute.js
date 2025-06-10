00ac8ef4ce4799dbcf5ebea0f7e720c8cc876217
westline95 <westline.1995@gmail.com>
2025-06-10 15:29:45 +0700

    param route delivery

--- server/routes/DeliveryRoute.js
+++ server/routes/DeliveryRoute.js
@@ -5,8 +5,8 @@ import DeliveryController from "../controllers/DeliveryController.js";
 const DeliveryRoute = express.Router();
 
 DeliveryRoute.post("/delivery", DeliveryController.createDelivery);
-DeliveryRoute.patch("/delivery/status", DeliveryController.updateStatusDelivery);
-DeliveryRoute.patch("/delivery/assign", DeliveryController.assignCourier);
+DeliveryRoute.patch("/delivery/status/:delivery_id", DeliveryController.updateStatusDelivery);
+DeliveryRoute.patch("/delivery/assign/:delivery_id", DeliveryController.assignCourier);
 DeliveryRoute.get("/delivery/all", DeliveryController.getAllDelivery);
 
 export default DeliveryRoute;
