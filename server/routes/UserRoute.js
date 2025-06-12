70bb7fd7cf1998ccee231831a389fa5de98d5de7
westline95 <westline.1995@gmail.com>
2025-06-12 15:56:08 +0700

    adding user courier role in user routee

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -6,6 +6,7 @@ import accessValidation from "../api/auth.js";
 const UserRoute = express.Router();
 
 UserRoute.get("/user/all", accessValidation, UserController.getUsers);
+UserRoute.get("/user/courier", accessValidation, UserController.getUserCourierRole);
 UserRoute.post("/user/write",accessValidation, UserController.insertUser);
 UserRoute.put("/user",accessValidation, UserController.updateUser);
 UserRoute.delete("/user",accessValidation, UserController.deleteUser);
