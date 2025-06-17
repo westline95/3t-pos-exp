7c441a8ec09ded97bc77f14c3ba5f4a25cadffde
westline95 <westline.1995@gmail.com>
2025-06-18 02:02:06 +0700

    patch /sales

--- server/routes/SalesRoute.js
+++ server/routes/SalesRoute.js
@@ -13,6 +13,7 @@ SalesRoute.get("/sales/cust/paytype/v2", SalesController.salesByOneCustPayType2)
 SalesRoute.post("/sales/write", SalesController.insertSales);
 SalesRoute.post("/sales/writes", SalesController.insertMultipleSales);
 SalesRoute.put("/sales", SalesController.updateSales);
+SalesRoute.patch("/sales", SalesController.updateSales);
 SalesRoute.patch("/sales/invs", SalesController.updateSalesAddInvoices);
 SalesRoute.patch("/sales/:order_id", SalesController.updateSalesAddInv);
 SalesRoute.patch("/sales/update/status", SalesController.updateOrderStatus);
