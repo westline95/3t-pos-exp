be50bc20bf4cb281e2c6d18db881eeb91b55c03e
westline95 <westline.1995@gmail.com>
2025-08-07 21:09:50 +0700

    allowed origin dev url

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'https://3t-main-app.netlify.app',
     'http://localhost:5173',
     'http://localhost:5050',
+    'http://192.168.100.8:5173',
 ];
 
 export default allowedOrigins;
