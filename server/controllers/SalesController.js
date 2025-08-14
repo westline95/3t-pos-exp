bd497ed704c1d3f64439dfa79b2e20b5b174a3f1
westline95 <westline.1995@gmail.com>
2025-08-14 15:39:06 +0700

    fix delete order credit:ro 3

--- server/controllers/SalesController.js
+++ server/controllers/SalesController.js
@@ -525,7 +525,8 @@ const checkNextCustSales = async(req, res) => {
         const getData = await AllModel.OrdersModel.findAll({
             where: {
                 customer_id: id,
-                order_date: { [Op.gte]: new Date(order_date) }
+                order_date: { [Op.gte]: new Date(order_date) },
+                order_status: 'canceled'
             },
             order: [["order_date", "ASC"]],
             include: [
