c1af3a27b062858d5bf2c1f6062c22961fc511b8
westline95 <westline.1995@gmail.com>
2025-09-01 12:33:03 +0700

    allowed origin whatsapp url

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://threet-pos-exp.onrender.com',
     'https://3t-main-app.netlify.app',
+    'https://graph.facebook.com/v22.0/785616564632395/messages',
     'http://localhost:5173',
     'http://localhost:5050',
     'http://192.168.100.8:5173',
