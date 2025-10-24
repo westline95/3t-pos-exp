a728e378136817704951e2acc7a20d1c751ab53f
westline95 <westline.1995@gmail.com>
2025-10-24 16:58:32 +0700

    added new allowed origin

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -5,6 +5,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5050',
     'http://192.168.100.8:5173',
+    'http://192.168.36.83:5173',
 ];
 
 export default allowedOrigins;
